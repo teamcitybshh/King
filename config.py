37789e507aba61364c2f7ce8cad6ded1427a4bd2
teamcitybshh <teamcity919@gmail.com>
2024-12-30 07:22:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7835087927:AAEmLFra06fKjsnFqrzUR9Obn98jMhnI1w4"
+ADMIN_IDS = ["5027022179"]
+GROUP_ID = -1002491362209  # Replace with your group ID
+GROUP_LINK = "https://t.me/kingvipddosgruap"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
